368ee43d4ac81d9077e4e1913c63c9feafe3d961
Nitchaudhary07 <niteshchaudhary491@gmail.com>
2024-05-21 14:03:19 +0530

    TODO is complate using vite

---
+++ 60_Day_git_assignment/React App_Assignments/Vite-CRA-useref/vite/my-vite-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoApp from './Component/TodoApp';
+
+const App = () => {
+  return (
+    <div>
+      <TodoApp />
+    </div>
+  );
+};
+
+export default App;
---
+++ 60_Day_git_assignment/React App_Assignments/Vite-CRA-useref/vite/my-vite-app/src/Component/TodoApp.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleAddTodo = () => {
+    if (newTodo.trim()) {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={handleInputChange}
+        placeholder="Add a new todo"
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoApp;
